eed4b544c3ab65ee42088e0c1f5b3cc720007878
anna belenko <66851299+annabelenko@users.noreply.github.com>
2024-10-30 15:49:42 -0400

    center div

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import React, { useState } from "react";
 import "./App.css";
 import { motion, useScroll, useSpring } from "framer-motion";
 import { Example } from "./components/Example";
+import { Navbar } from "./components/Navbar"; // Import Navbar
 
 export default function App() {
   const { scrollYProgress } = useScroll();
   const scaleX = useSpring(scrollYProgress, {
     stiffness: 100,
     damping: 30,
-    restDelta: 0.001
+    restDelta: 0.001,
   });
 
   const [count, setCount] = useState(0);
@@ -21,15 +22,12 @@ export default function App() {
 
   return (
     <>
+      <Navbar /> {/* Add Navbar Component */}
       <motion.div className="progress-bar" style={{ scaleX }} />
-      <h1>
-        <code>useScroll</code> with spring smoothing
-      </h1>
+
       <div className="example-container">
         <Example key={count} />
       </div>
     </>
   );
 }
-
-
