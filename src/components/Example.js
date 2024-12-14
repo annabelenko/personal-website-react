524c7468a9e1d2c36146d45c8bb42b102b8224a4
anna belenko <66851299+annabelenko@users.noreply.github.com>
2024-12-13 23:21:28 -0500

    small changes

--- src/components/Example.js
+++ src/components/Example.js
@@ -1,4 +1,3 @@
-// src/components/Example.js
 import * as React from "react";
 import { useRef } from "react";
 import { motion } from "framer-motion";
@@ -9,16 +8,24 @@ export const Example = () => {
   const constraintsRef = useRef(null);
 
   return (
-    <div className="example-container" ref={constraintsRef}>
-      <motion.div className="drag-area" />
-      <motion.img 
-        src={Smiley} 
-        alt="Smiley face" 
-        className="smiley-face" 
-        drag 
-        dragConstraints={constraintsRef} 
-      />
-      <p className="example-text"> This is some text</p>
+    <div className="example-container">
+      {/* Smiley Section */}
+      <div className="smiley-container" ref={constraintsRef}>
+        <motion.img 
+          src={Smiley} 
+          alt="Smiley face" 
+          className="smiley-face" 
+          drag 
+          dragConstraints={constraintsRef} 
+        />
+      </div>
+
+      {/* Text Section */}
+      <div className="text-container">
+        <p className="example-text">Hello, I'm</p>
+        <h1 className="example-text">ANNA</h1>
+        <p className="example-subtitle">A passionate learner in tech!</p>
+      </div>
     </div>
   );
 };
